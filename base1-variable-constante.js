d2baebffa7ebda19f41100dbd4f342aa2ca6b61c
medharik <med.harik@gmail.com>
2024-10-26 12:12:15 +0100

    base 1 - scope var, let const  JS

---
+++ base1-variable-constante.js
@@ -0,0 +1,20 @@
+{ 
+    //scope : global , local
+let x=2;
+let prenom="ali";
+// console.log("salut ",x,"jfhdjfh",5,eori9);
+// console.log(x,7,"salut")
+{
+    TVA=20;
+    var z=8;
+    let y=x++ * 3;
+    {
+        let a=9;
+        const TVA=20;
+        console.log(TVA);
+    }
+         console.log('a',a,'x',x,'z',z,'y',y);
+
+         console.log('x',x,'y',y);
+}
+}
---
+++ base2.js
@@ -0,0 +1,6 @@
+let x=2;//x=2
+let y=x++*3;//x=3,y=6
+let z=++x* 3;//x=4,z=12
+console.log(x,y,z);
+// R1 : x=3,y=9,z=?
+//R2 : x=4,y=6,z=12
